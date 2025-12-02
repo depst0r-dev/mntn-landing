a2067568c8b1a37a9901219ecdf159eb6111422e
Sergey Panteleenko <depstor90@gmail.com>
2025-12-03 01:55:35 +0300

    hero-img-colection

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 import '/src/scss/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+// window.addEventListener('scroll', () => {
+//     const scrolled = window.pageYOffset;
+//     const layers = document.querySelectorAll('.parallax-layer');
+
+//     layers.forEach(layer => {
+//         let speed = 0.1;
+//         if (layer.classList.contains('layer-mountains')) speed = 0.3;
+//         if (layer.classList.contains('layer-foreground')) speed = 0.5;
+
+//         const yPos = -(scrolled * speed);
+//         layer.style.transform = `translateY(${yPos}px)`;
+//     });
+// });
